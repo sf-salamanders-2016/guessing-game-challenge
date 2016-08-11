d32d8ed4edbe8f9c5fa8af9d4f600bf0d1323979
Assunta Crux & Jeff Roy <pair+assunta09.jeffreyroy@devbootcamp.com>
2016-08-11 11:08:03 -0700

    Add methods to GuessingGame

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,14 +1,34 @@
 class GuessingGame
   attr_accessor :congrats_message
+  attr_reader :remaining_guesses, :has_won?
 
   def initialize(secret_number, allowed_guesses)
     @secret_number = secret_number
-    @allowed_guesses = allowed_guesses
-    @guesses_count = 0
+    @remaining_guesses = allowed_guesses
     @congrats_message = "Yay, you won!"
+
   end
 
+  def has_lost?
+    @remaining_guesses == 0
+  end
 
+  def has_won?
+    @current_guess == secret_number
+  end
+
+  def guess(number)
+    @current_guess = number
+    @remaining_guesses -= 1
+    if number == @secret_number
+      puts @congrats_message
+    elsif number < @secret_number
+      puts "Too low!"
+    else
+      puts "Too high!"
+    end
+
+  end
 
 
 end
